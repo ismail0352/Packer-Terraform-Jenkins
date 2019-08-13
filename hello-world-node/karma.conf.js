79753241630f7f5b29b3e306973022c617a0fa7a
Ismail <ismail.2104@gmail.com>
2019-08-13 18:31:21 +0530

    Updated Jenkinsfile and testing karma

--- hello-world-node/karma.conf.js
+++ hello-world-node/karma.conf.js
@@ -25,18 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ]
-      }
-    },
+    browsers: ['ChromeHeadless'],
+    // customLaunchers: {
+    //   ChromeHeadlessCI: {
+    //     base: 'ChromeHeadless',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
     singleRun: true,
     restartOnFileChange: true
   });
